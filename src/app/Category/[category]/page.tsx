6fb2ba1f183adf83fe3330f7027d7829aadba3c5
Mohamed Basyuny <muhammadbasyunydev@gmail.com>
2025-06-10 03:25:53 +0300

    Updating the Files

--- src/app/Category/[category]/page.tsx
+++ src/app/Category/[category]/page.tsx
@@ -1,4 +1,5 @@
-const CategoryItem = ({ params }: any) => {
+type params = { category: string };
+const CategoryItem = ({ params }: { params: params }) => {
   return <h1 className="font-bold text-center">{params.category}</h1>;
 };
 
