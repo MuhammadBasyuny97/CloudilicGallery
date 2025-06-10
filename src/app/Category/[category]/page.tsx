d57a70a8507e5a60e047c50c63ba4a9991f458cf
Mohamed Basyuny <muhammadbasyunydev@gmail.com>
2025-06-10 03:32:01 +0300

    Updating 2

--- src/app/Category/[category]/page.tsx
+++ src/app/Category/[category]/page.tsx
@@ -1,5 +1,9 @@
-type params = { category: string };
-const CategoryItem = ({ params }: { params: params }) => {
+type Props = {
+  params: {
+    category: string;
+  };
+};
+const CategoryItem = ({ params }: Props) => {
   return <h1 className="font-bold text-center">{params.category}</h1>;
 };
 
